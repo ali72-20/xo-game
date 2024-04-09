546aada526f882c9e208f2236c8589ce9f71b209
ali72-20 <as20201118@gmail.com>
2024-04-10 00:51:02 +0200

    + design home screen + valid input

--- app/src/main/java/com/example/xo/MainActivity.kt
+++ app/src/main/java/com/example/xo/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.xo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.xo.databinding.ActivityMainBinding
+import com.example.xo.databinding.ActivitySplashScreenBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        startGame()
+    }
+
+    private fun startGame() {
+        viewBinding.contentHome.startBtn.setOnClickListener {
+
+        }
+    }
+
+    private fun valideinput() : Boolean{
+        var isValid : Boolean = true;
+        val firstName:String = viewBinding.contentHome.firstInputEt.text.toString()
+        val secondName: String = viewBinding.contentHome.secondInputEt.text.toString()
+        if(firstName.isBlank()){
+            viewBinding.contentHome.firstInputTil.error = "Please enter first player"
+            isValid = false
+        }else{
+            viewBinding.contentHome.firstInputTil.error = null
+        }
+
+        if(secondName.isBlank()){
+            viewBinding.contentHome.secondInputTil.error = "Please enter second player"
+            isValid = false
+        }else{
+            viewBinding.contentHome.secondInputTil.error = null
+        }
+        return isValid
     }
 }
--- app/src/main/java/com/example/xo/startApp/SplashScreen.kt
+++ app/src/main/java/com/example/xo/startApp/SplashScreen.kt
@@ -1,12 +1,28 @@
 package com.example.xo.startApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.xo.MainActivity
 import com.example.xo.R
+import com.example.xo.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    lateinit var viewBinging : ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        viewBinging = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(viewBinging.root)
+
+        Handler(mainLooper).postDelayed({
+            startApp()
+        },2000)
     }
+
+    private fun startApp(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
